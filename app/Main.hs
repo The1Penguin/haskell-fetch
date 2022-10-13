834fd53d884bee7e590f5ce4513450c9ac32b347
Pingu <nor@acorneroftheweb.com>
2022-10-13 23:29:10 +0200

    Switched to let in statement instead of where

--- app/Main.hs
+++ app/Main.hs
@@ -14,9 +14,11 @@ hostname :: IO String
 hostname = getHostName
 
 distro :: IO String
-distro = liftM (\x -> x =~ distroPattern :: String) $ liftM (\x -> x =~ linePattern :: String) $ readFile "/etc/os-release"
-  where distroPattern = "(?!\")[A-z\\d\\s\\/]+(?=\")"
-        linePattern = "^NAME=.*"
+distro = let
+    distroPattern = "(?!\")[A-z\\d\\s\\/]+(?=\")"
+    linePattern = "^NAME=.*"
+    in
+      liftM (\x -> x =~ distroPattern :: String) $ liftM (\x -> x =~ linePattern :: String) $ readFile "/etc/os-release"
 
 main :: IO ()
 main = do

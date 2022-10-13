66ea8e5cb8f15f02a4ccb167f8ed13cc1a523a82
Pingu <nor@acorneroftheweb.com>
2022-10-13 21:48:26 +0200

    Really hacky way of getting distro

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import System.Environment
 import Network.HostName
 import Control.Monad
+import System.IO
+import Data.List
 
 user :: IO String
 user = getEnv "USER"
@@ -10,5 +12,11 @@ user = getEnv "USER"
 hostname :: IO String
 hostname = getHostName
 
+distro :: IO String
+distro = liftM (reverse . tail . reverse . (drop 6) . (!! 0) . filter (isPrefixOf "NAME=") . lines) $ readFile "/etc/os-release"
+
+
+
 main :: IO ()
-main = putStrLn =<< liftM2 (\x y -> x ++ "@" ++ y) user hostname
+main = do
+  putStrLn =<< liftM3 (\x y z -> x ++ "@" ++ y ++ "\n" ++ z) user hostname distro
